7e1b0e31e2156b5980bff9e9d720f41d6d2fc6c5
unknown <ANDRE.MARQUES@UVA.LOCAL>
2024-03-25 17:21:54 -0300

    DEBUG

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # adicionar essa tag para que nosso projeto encontre o .env
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-DEBUG = os.getenv("DEBUG") == 'True'
+DEBUG = os.getenv("DEBUG")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
