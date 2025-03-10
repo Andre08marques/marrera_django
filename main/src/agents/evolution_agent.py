44a8279a968ff3168cc35ff65f3a0f024bbfbe1e
andre <andre.ti.marques@gmail.com>
2025-03-10 15:47:11 -0300

    ajuste recreate

--- main/src/agents/evolution_agent.py
+++ main/src/agents/evolution_agent.py
@@ -41,32 +41,46 @@ class Evolution():
             )
     
     def instance_recreate(self, name, key):
-        hash = gerar_chave()
-        headers = {
-            "apikey": f"{self.__evolutionmasterkey}"
-        }
+        try:
+            headers = {
+                "apikey": f"{self.__evolutionmasterkey}"
+            }
 
-        json = {
-            "token": f"{self.__evolutionmasterkey}",
-            "groups_ignore": "false",
-        }
-        
-        response = requests.post(
-            url=f"{self.__base_url}/instance",
-            headers=headers,
-            json=json
-        )
-        status_code = response.status_code
-        if ((status_code >= 200) and (status_code <= 299)):
-            data = {
+            json = {
+                "token": f"{self.__evolutionmasterkey}",
+                "groups_ignore": "false",
+            }
+            
+            response = requests.post(
+                url=f"{self.__base_url}/instance",
+                headers=headers,
+                json=json,
+                timeout=15  
+            )
+            response.raise_for_status() 
+            
+            return {
                 "status_code": response.status_code,
                 "response": response.json()
             }
-            return data
-        else: 
-            raise HttpErrors(
-                message=response.json()['response']['message'], status_code=status_code
-            )
+        except requests.exceptions.Timeout:
+            raise HttpErrors(message="A requisição expirou. O servidor pode estar indisponível.", status_code=504)
+        
+        except requests.exceptions.ConnectionError:
+            raise HttpErrors(message="Erro de conexão com a API. O servidor pode estar fora do ar.", status_code=503)
+
+        except requests.exceptions.HTTPError as e:
+            try:
+                error_response = response.json()
+            except requests.exceptions.JSONDecodeError:
+                error_response = "Erro desconhecido na API"
+            raise HttpErrors(message=error_response, status_code=response.status_code)
+
+        except requests.exceptions.RequestException as e:
+            raise HttpErrors(message=f"Erro na requisição: {str(e)}", status_code=500)
+        
+        
+       
             
     
     def instance_status(self, name, key):
